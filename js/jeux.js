247a31772c4c23d79c0598912677a21c16033e52
ghollamsimo <ghollamsimo1@gmail.com>
2024-08-30 16:37:24 +0100

    Adding when i click in the any cell saved the X or O and i remove the listner when finish the process

--- js/jeux.js
+++ js/jeux.js
@@ -1,11 +1,23 @@
-const first_player = document.getElementById('player_one').value
-const second_player = document.getElementById('player_two').value
+let first_player = '';
+let second_player = '';
+let users = [];
+const playersData = JSON.parse(localStorage.getItem('players'));
+const playerOne = document.getElementById('player_one')
+const playerTwo = document.getElementById('player_two')
+if (playersData && playersData.length > 0) {
+      first_player = playersData[0].first_player;
+      second_player = playersData[0].second_player;
+      playerOne.innerHTML = first_player
+      playerTwo.innerHTML = second_player
+} else {
+      console.log('No player data found in localStorage.');
+}
 
 function validation_form(){
 document.getElementById('form').addEventListener('submit' , function(event) {
     const player_regex = /^[a-zA-Z]+$/;
-
-
+    first_player = document.getElementById('player_one').value;
+    second_player = document.getElementById('player_two').value;
     if (!player_regex.test(first_player)) {
         const error = document.getElementById('error_message')
         error.innerHTML = "You need to enter the name of the player one !"
@@ -17,32 +29,35 @@ document.getElementById('form').addEventListener('submit' , function(event) {
         error.innerHTML = "You need to enter the name of the player two !"
         event.preventDefault();
     }
+    save_localStorage()
+
+    document.getElementById('form').removeEventListener('submit', event);
 
 })
-    save_localStorage()
 }
-
-let winnig = true
-let draw = '';
-let message = '';
-
-if (first_player || second_player === winnig){
-    message = `Player ${first_player || second_player} winnig !`
-}else if (first_player && second_player === draw){
-    message = 'Game ended in Draw';
-}
-
-const saving = [{
-    'first_player': first_player,
-    'second_player': second_player,
-    'winner': first_player ? first_player : second_player,
-    'message': message,
-    'first_player_score': 0,
-    'second_player_score': 0,
-}];
-
 function save_localStorage() {
+    first_player = document.getElementById('player_one').value;
+    second_player = document.getElementById('player_two').value;
+
+    const saving = [{
+        'first_player': first_player,
+        'second_player': second_player,
+        'winner': first_player ? first_player : second_player,
+        'first_player_score': 0,
+        'second_player_score': 0,
+    }];
+
     localStorage.setItem('players', JSON.stringify(saving))
+}
+
+let player_turn = 'x'
+function handleClick(e){
+    let clickedCell = e.target
+    if (clickedCell.textContent === '') {
+        clickedCell.textContent = player_turn;
+        player_turn = player_turn === 'x' ? 'o' : 'x';
+    }
+
 }
 
 const cell_selector = document.querySelector('.row')
@@ -50,11 +65,11 @@ for (let i = 1; i <= 400; i++) {
     const create_cell = document.createElement('div')
     create_cell.className = 'cell'
     create_cell.setAttribute('data-cell-index', i);
+    create_cell.addEventListener('click', handleClick);
+
     cell_selector.appendChild(create_cell)
 }
 
-const Plaayer_X_turn = 'x'
-const Plaayer_O_turn = 'o'
 const Winnig_combination = []
 
 for (let i = 1; i <= 400; i++) {
